3b2b335e5a61b887ef7b337a6cb2561b10ee1aa1
Connor Bunch <connorbunch@outlook.com>
2020-02-22 16:44:51 -0700

    working with slack routes

--- routes/slack.js
+++ routes/slack.js
@@ -2,13 +2,17 @@ var slackService = require('../services/slack');
 
 exports.receive = function (req, res, next) {
     console.log(req.body);
-    slackService.handle(req.body, function(message) {
-        if (message) {
-            res.send(message);
-        } else {
-            //doesn't matter what we send back to slack. Just needs to be a 2xx code within three seconds...
-            res.send('{"Message":"Thanks partner!"')
-        }
-    });
+    //If slack is giving us a URL verification challenge, we need to respond with the challenge value
+    if (req.body["type"] == "url_verification") {
+        res.send(req.body["challenge"]);
+    }
+    // slackService.handle(req.body, function(message) {
+    //     if (message) {
+    //         res.send(message);
+    //     } else {
+    //         //doesn't matter what we send back to slack. Just needs to be a 2xx code within three seconds...
+    //         res.send('{"Message":"Thanks partner!"')
+    //     }
+    // });
     next();
 };
